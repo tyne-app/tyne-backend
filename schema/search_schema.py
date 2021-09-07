a7f9b30cf22263b0554e028ac12333176082b6b4
=Antonio Rodríguez Mejías <tonyn.rome@gmail.com>
2021-09-07 14:03:45 -0300

    Rating to flaot

--- schema/search_schema.py
+++ schema/search_schema.py
@@ -15,7 +15,7 @@ class PreviewBranch(BaseModel):
     name: str
     image_url: Optional[str] = None  # TODO: Definir si es oopcional
     price: Optional[int] = 0  # TODO: Descartar Optional, por ahora se deja por poca e inconsistente data
-    rating: Optional[int] = 0
+    rating: Optional[float] = 0
     description: Optional[str] = None  # TODO: Creo que es opcional, por confirmar
 
 
