94699ce101d19b62907590b11c8b31ab64c14d25
jsarabia.dev <jsarabia.dev@gmail.com>
2021-11-25 18:28:23 -0300

    Add constants for responses messages

--- util/Constants.py
+++ util/Constants.py
@@ -1,5 +1,8 @@
 class Constants:
 
-    # Errors messages
+    # ERRORS MESSAGES
     INTERNAL_ERROR = "INTERNAL SERVER ERROR"
     INTERNAL_ERROR_DETAIL = "Ha ocurrido un error, por favor inténtelo de nuevo más tarde"
+
+    MENU_READ_ERROR = "Error al obtener menu"
+    MENU_READ_ERROR_DETAIL = "Productos no encontrados para el menu"
