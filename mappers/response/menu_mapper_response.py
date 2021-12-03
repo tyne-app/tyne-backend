b326ca9cd8c20e46ce6245c17a0121e73a3c0774
David Moya <dav.moyam@outlook.cl>
2021-12-03 14:30:05 -0300

    fix

--- mappers/response/menu_mapper_response.py
+++ mappers/response/menu_mapper_response.py
@@ -1,11 +1,11 @@
 from domain2.Menu import Menu
-
 from repository.entity.BranchEntity import BranchEntity
 from repository.entity.ProductEntity import ProductEntity
 
 
 def to_menu_read_response(products: list[ProductEntity], branch: BranchEntity, opinions):
-    return Menu.to_menu_read_domain(products, branch, opinions)
+    menu = Menu()
+    return menu.to_menu_read_domain(products, branch, opinions)
 
 
 def to_menu_create_response():
