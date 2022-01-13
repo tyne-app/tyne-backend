f99ef600290e8097f1cbb71f0456333ddaac30df
David Moya <dav.moyam@outlook.cl>
2022-01-12 22:27:48 -0300

    Now main.py is in root directory

--- src/configuration/firebase/firebase_config.py
+++ src/configuration/firebase/firebase_config.py
@@ -7,7 +7,7 @@ class FirebaseConfig:
     def init_firebase(self):
         if len(firebase_admin._apps) == 0:
             cred_obj = firebase_admin.credentials.Certificate(
-                os.path.abspath("./configuration/firebase/firebase_credentials.json"))
+                os.path.abspath("./src/configuration/firebase/firebase_credentials.json"))
 
             firebase_admin.initialize_app(cred_obj, {
                 'projectId': "tyne-app",
