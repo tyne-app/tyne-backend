ee3d38c9f460d9a5aec945602ac9dd552313d506
=Antonio Rodríguez Mejías <tonyn.rome@gmail.com>
2022-02-20 18:08:40 -0300

    [FIX] State active user

--- src/dto/request/ClientRequest.py
+++ src/dto/request/ClientRequest.py
@@ -20,7 +20,7 @@ class ClientRequest(BaseModel):
         user_entity = UserEntity()
         user_entity.email = self.email
         user_entity.password = self.password
-        user_entity.is_active = False
+        user_entity.is_active = True
         user_entity.id_user_type = UserType.CLIENT
         user_entity.created_date = datetime.now(tz=timezone.utc)
         return user_entity
