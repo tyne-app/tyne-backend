42251cd31aa6d71ba573c9aaa2e037e01352fb60
=Antonio Rodríguez Mejías <tonyn.rome@gmail.com>
2022-02-26 22:54:43 -0300

    [UPDATE] links khipu

--- src/service/KhipuService.py
+++ src/service/KhipuService.py
@@ -25,9 +25,9 @@ class KhipuService:
             'bank_id': '',
             'transaction_id': transaction_id,
             'custom': '',
-            'notify_url': 'https://tyne-app.herokuapp.com/estado-pago/exitoso',#self._settings_.KHIPU_NOTIFY_URL,
-            'return_url': 'https://tyne-app.herokuapp.com/estado-pago/exitoso', #self._settings_.KHIPU_RETURN_URL,
-            'cancel_url': 'https://tyne-app.herokuapp.com/estado-pago/cancelado',#self._settings_.KHIPU_CANCEL_URL,
+            # 'notify_url': 'http://localhost:4200/estado-pago/exitoso',#self._settings_.KHIPU_NOTIFY_URL,
+            'return_url': 'http://localhost:4200/estado-pago/exitoso', #self._settings_.KHIPU_RETURN_URL,
+            'cancel_url': 'http://localhost:4200/estado-pago/cancelado',#self._settings_.KHIPU_CANCEL_URL,
             'picture_url': self._settings_.KHIPU_PICTURE_URL,
             'currency': 'CLP',
             'notify_api_version': '1.3'
