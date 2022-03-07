2f9680ce337509cd5ce32c7dc23c1960a42c336d
AndromedaK <silverhaloo@gmail.com>
2022-03-07 19:18:15 -0300

    TYNE-211 [remove] preference field and setup by default

--- src/dto/request/NewReservationRequest.py
+++ src/dto/request/NewReservationRequest.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Optional
 from starlette import status
 
 from src.util.Constants import Constants
@@ -19,7 +19,7 @@ class NewReservationRequest(BaseModel):
     people: int
     date: datetime
     hour: str
-    preference: str
+    preference: Optional[str] = "Interior"
     products: list[ProductRequest]
 
     def get_products_ids(self):
