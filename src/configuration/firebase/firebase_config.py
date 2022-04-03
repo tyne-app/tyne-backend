d6ae2172f81e4efa44dc39969f47342759e00ea7
David Martin <martin.david.dev@gmail.com>
2022-04-03 17:13:03 -0400

    fix firebase config directory path

--- src/configuration/firebase/firebase_config.py
+++ src/configuration/firebase/firebase_config.py
@@ -7,7 +7,7 @@ class FirebaseConfig:
     def init_firebase(self):
         if len(firebase_admin._apps) == 0:
             cred_obj = firebase_admin.credentials.Certificate(
-                os.path.abspath("./src/configuration/firebase/firebase_credentials.json"))
+                os.path.abspath("./firebase_credentials.json"))
 
             firebase_admin.initialize_app(cred_obj, {
                 'projectId': Settings.FIREBASE_PROJECT_ID,
