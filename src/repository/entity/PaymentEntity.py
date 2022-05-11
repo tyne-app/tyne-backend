6a259525009cd0ee93d82f47bfdd906dbd925e50
YJLC11 <yerko.95lc@gmail.com>
2022-05-11 01:10:32 -0400

    TYNE-291: - Add column in table payment, save id payment by mercado pago

--- src/repository/entity/PaymentEntity.py
+++ src/repository/entity/PaymentEntity.py
@@ -13,6 +13,7 @@ class PaymentEntity(Base):
     method = Column(String)
     amount = Column(Integer)
     receipt_url = Column(String)
+    payment_mp_id = Column(Integer)
 
     type_coin_id = Column(Integer)
     reservation_id = Column(Integer, ForeignKey("tyne.reservation.id"))
