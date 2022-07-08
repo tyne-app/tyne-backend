69e4902852b19c672ec0795f51c8143e5031afd9
David Martin <martin.david.dev@gmail.com>
2022-07-08 18:48:44 -0400

    TYNE-370: - modified max age cache

--- src/controller/territory_controller.py
+++ src/controller/territory_controller.py
@@ -22,13 +22,14 @@ _country_dao_ = CountryDao()
     status_code=status.HTTP_200_OK
 )
 async def get_countries(response: Response, db: Session = Depends(database.get_data_base)):
+    response.headers["Cache-Control"] = "public, max-age=604800"
+
     countries = _country_dao_.get_all_countries(db)
 
     if len(countries) == 0:
         response.status_code = status.HTTP_204_NO_CONTENT
         return response
 
-    response.headers["Cache-Control"] = "public, max-age=3600"
     return countries
 
 
@@ -37,13 +38,14 @@ async def get_countries(response: Response, db: Session = Depends(database.get_d
     status_code=status.HTTP_200_OK
 )
 async def get_country(response: Response, id_country: int, db: Session = Depends(database.get_data_base)):
+    response.headers["Cache-Control"] = "public, max-age=604800"
+
     country = _country_dao_.get_country_by_id(id_country, db)
 
     if country is None:
         response.status_code = status.HTTP_204_NO_CONTENT
         return response
 
-    response.headers["Cache-Control"] = "public, max-age=3600"
     return country
 
 
@@ -52,13 +54,12 @@ async def get_country(response: Response, id_country: int, db: Session = Depends
     status_code=status.HTTP_200_OK
 )
 async def get_cities_by_country(response: Response, id_country: int, db: Session = Depends(database.get_data_base)):
+    response.headers["Cache-Control"] = "public, max-age=604800"
     cities = _city_dao_.get_cities(id_country, db)
 
     if len(cities) == 0:
         response.status_code = status.HTTP_204_NO_CONTENT
         return response
-
-    response.headers["Cache-Control"] = "public, max-age=3600"
     return cities
 
 
@@ -67,13 +68,13 @@ async def get_cities_by_country(response: Response, id_country: int, db: Session
     status_code=status.HTTP_200_OK
 )
 async def get_city(response: Response, id_city: int, db: Session = Depends(database.get_data_base)):
+    response.headers["Cache-Control"] = "public, max-age=604800"
     city = _city_dao_.get_city_by_id(id_city, db)
 
     if city is None:
         response.status_code = status.HTTP_204_NO_CONTENT
         return response
 
-    response.headers["Cache-Control"] = "public, max-age=3600"
     return city
 
 
@@ -82,18 +83,19 @@ async def get_city(response: Response, id_city: int, db: Session = Depends(datab
     status_code=status.HTTP_200_OK
 )
 async def get_states_by_city(response: Response, city_id: int, db: Session = Depends(database.get_data_base)):
+    response.headers["Cache-Control"] = "public, max-age=604800"
     states = _state_dao_.get_states(city_id=city_id, db=db)
 
     if len(states) == 0:
         response.status_code = status.HTTP_204_NO_CONTENT
         return response
 
-    response.headers["Cache-Control"] = "public, max-age=3600"
     return states
 
 
 @territory_controller.get('/cities/{city_id}/states/available', status_code=status.HTTP_200_OK)
 def get_available_states_by_city(response: Response, city_id: int, db: Session = Depends(database.get_data_base)):
+    response.headers["Cache-Control"] = "public, max-age=900"
     available_states = _state_dao_.get_available_states(city_id=city_id, db=db)
 
     if len(available_states) == 0:
@@ -108,11 +110,10 @@ def get_available_states_by_city(response: Response, city_id: int, db: Session =
     status_code=status.HTTP_200_OK
 )
 async def get_state(response: Response, id_state: int, db: Session = Depends(database.get_data_base)):
+    response.headers["Cache-Control"] = "public, max-age=604800"
     state = _state_dao_.get_state_by_id(id_state, db)
 
     if state is None:
         response.status_code = status.HTTP_204_NO_CONTENT
         return response
-
-    response.headers["Cache-Control"] = "public, max-age=3600"
     return state
